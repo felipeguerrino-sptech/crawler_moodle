fbe7786e35ade200c0350d0f0496328000efe25f
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-10-25 11:55:45 -0300

    refatoracao: usando selenium (headless) para login e download

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-import requests
-import wget
+from time import sleep
+
+import getpass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def main():
     ra = input('Insira o RA: ')
-    senha = input('Insira a senha: ')
-    cookie = get_cookie(ra, senha)
-    token = get_github_token(cookie)
-    print(token)
+    senha = getpass.getpass(prompt='Insira a senha: ')
+    get_github_token(ra, senha)
+    
 
-def get_cookie(ra, senha):
-    s = requests.Session()
-    login = s.post('https://moodle.sptech.school/login/index.php', params={'ra': '02231048', 'senha': '#Gf50008690871'})
+def get_github_token(ra, senha):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_experimental_option('prefs', {'download.default_directory': '/home/aluno/temp'})
 
-    return login.cookies['MoodleSession']
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://moodle.sptech.school/")
+    
+    username = driver.find_element(By.ID, 'username')
+    username.send_keys(ra)
+    password = driver.find_element(By.ID, 'password')
+    password.send_keys(senha)
+    driver.find_element(By.ID, 'loginbtn').click()
+    sleep(1)
+    driver.find_element(By.CLASS_NAME, 'icon').click() 
+    driver.find_element(By.XPATH, '//a[@href="https://moodle.sptech.school/user/files.php"]').click()    
+    assert 'Arquivos privados' in driver.title
+    sleep(5)
+    driver.find_element(By.XPATH, "//div[text()='github.txt']").click()
+    driver.find_element(By.CLASS_NAME, 'fp-file-download').click()
+    sleep(2)
+    driver.close()
 
-def get_github_token(cookie):
-    s = requests.Session()
-    res = wget.download('https://moodle.sptech.school/draftfile.php/20653/user/draft/727639391/github.txt')
-
-    print(res.status_code)
-    return res.content
 if __name__ == '__main__':
     main()
